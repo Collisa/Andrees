842185af0ddc479e74769fb1766edff7c4c626d4
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2020-12-23 19:53:01 +0100

    added tooltip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: {
-    content: [
-      '*.html',
-    ]
+    content: ["templates/*.html", "templates/**/*.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -13,19 +11,19 @@ module.exports = {
         teal: colors.teal,
       },
       maxHeight: {
-        '112': '28rem',
-        '128': '32rem',
-        '144': '36rem',
+        112: "28rem",
+        128: "32rem",
+        144: "36rem",
       },
     },
   },
   variants: {
     extend: {
-      ringWidth: ['hover', 'active'],
-      zIndex: ['hover', 'active'],
-      borderWidth: ['hover', 'focus'],
-      backgroundColor: ['odd'],
+      ringWidth: ["hover", "active"],
+      zIndex: ["hover", "active"],
+      borderWidth: ["hover", "focus"],
+      backgroundColor: ["odd"],
     },
   },
   plugins: [],
-}
+};
